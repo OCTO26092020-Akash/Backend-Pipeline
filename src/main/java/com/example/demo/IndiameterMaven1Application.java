c82801a6e764d4ee6b2c881d4f0e3fd3053836dd
Akash-Tawade <akash1493>
2021-01-07 18:02:07 +0530

    first commit

---
+++ src/main/java/com/example/demo/IndiameterMaven1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiameterMaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiameterMaven1Application.class, args);
+	}
+
+}
